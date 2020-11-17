3a01cfee19b04f7418b7552d2a10778440eeb7c0
Jonathan Steele <jonathan@steele.pro>
2020-11-17 18:59:29 +0000

    feat: Functioning demo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 import '../styles/tailwind.css'
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+
+  return (
+    <div>
+      <ul>
+        {router.locales.map((locale) => (
+          <li key={locale}>
+            <Link href={router.asPath} locale={locale}>
+              <a>{locale}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default App
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,48 @@
-function IndexPage() {
-  return <div>Hello world!</div>
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { gql } from 'graphql-request'
+
+import graphCmsClient from '../lib/graphCmsClient'
+
+function IndexPage({ products }) {
+  const router = useRouter()
+
+  return (
+    <div>
+      {products.map((product) => (
+        <div key={product.id}>
+          <Link href={product.id} locale={router.locale}>
+            <a>
+              <h1>{product.name}</h1>
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export async function getStaticProps({ locale }) {
+  const { products } = await graphCmsClient.request(
+    gql`
+      query IndexPageQuery($locale: Locale!) {
+        products(locales: [$locale]) {
+          id
+          locale
+          name
+          price
+          slug
+        }
+      }
+    `,
+    { locale }
+  )
+
+  return {
+    props: {
+      products,
+    },
+  }
 }
 
 export default IndexPage
